3f0a85263d2273641949c1a9e21b8ac25969b5ce
Ana Belén <ana.b.95@hotmail.com>
2023-05-07 19:39:43 +0200

    carpeta db y helpers

---
+++ helpers.js
@@ -0,0 +1,22 @@
+const fs = require("fs/promises");
+
+//Genera errores personalizados que pueden enviarse como respuesta a una solicitu HTTP en caso de error en el server
+const generateError = (message, status) => {
+  const error = new Error(message);
+  error.httpStatus = status;
+  return error;
+};
+
+//Comprueba si un directorio existe en el sistema de archivos y lo crea sino.
+const createPathIfNotExists = async (path) => {
+  try {
+    await fs.access(path);
+  } catch {
+    await fs.mkdir(path);
+  }
+};
+
+module.exports = {
+  generateError,
+  createPathIfNotExists,
+};
