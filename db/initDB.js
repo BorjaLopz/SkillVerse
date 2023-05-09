46311a92765eb9d7531d4468dec177dfad2d0d21
Francisco Esparraga <fesparal@gmail.com>
2023-05-09 14:01:57 +0200

    initDBmodificaciones

--- db/initDB.js
+++ db/initDB.js
@@ -33,14 +33,14 @@ async function main() {
     CREATE TABLE users(
       id INT AUTO_INCREMENT PRIMARY KEY,
       email VARCHAR(100) NOT NULL UNIQUE,
-      nickname VARCHAR(30) NOT NULL UNIQUE,
+      nickname VARCHAR(30) NOT NULL UNIQUE (LENGTH(password) >= 4),
       name VARCHAR(30),
       surname VARCHAR(60),
-      password VARCHAR(100) NOT NULL,
+      password VARCHAR(100) NOT NULL CHECK (LENGTH(password) >= 8 AND password REGEXP '[A-Z]' AND password REGEXP '[a-z]' AND password REGEXP '[0-9]')
       biography VARCHAR(600),
       userPhoto VARCHAR(1000),
-      RRSS VARCHAR(1000),
-      active BOOLEAN DEFAULT FALSE
+      linkedin VARCHAR(100) CHECK (linkedin REGEXP '^https?://(www\.)?linkedin\.com/in/[\w-]+$')
+      active BOOLEAN DEFAULT TRUE,
     );
     `);
     /*RRSS Será actualizado de cara al 3º proyecto para poder guardar por separado las diferentes redes sociales 
@@ -49,14 +49,17 @@ async function main() {
     await connection.query(`
     CREATE TABLE requiredS(
       id INT AUTO_INCREMENT PRIMARY KEY,
-      title VARCHAR(50) NOT NULL,
+      title VARCHAR(50) NOT NULL (LENGTH(title) >= 15),
       request_body VARCHAR(500) NOT NULL CHECK (LENGTH(request_body) >= 15),
       user_id INT NOT NULL,
       file_name VARCHAR(90),
       creation_date DATETIME DEFAULT CURRENT_TIMESTAMP,
       required_type VARCHAR(20) NOT NULL,
       done BOOLEAN DEFAULT FALSE,
+      hide BOOLEAN DEFAULT FALSE,
       FOREIGN KEY (user_id) REFERENCES users (id)
+      FOREIGN KEY (file_name) REFERENCES users (id)
+      FOREIGN KEY (file_name) REFERENCES requiredS (id)
     );
     `);
 
@@ -67,10 +70,13 @@ async function main() {
       requiredS_id INT NOT NULL,
       creation_date DATETIME DEFAULT CURRENT_TIMESTAMP,
       serviceFile VARCHAR(30),
+      choseen BOOLEAN DEFAULT FALSE,
+      hide BOOLEAN DEFAULT FALSE,
       FOREIGN KEY (serviceFile) REFERENCES requiredS (id),
       FOREIGN KEY (serviceFile) REFERENCES users (id),
       FOREIGN KEY (user_id) REFERENCES users (id),
       FOREIGN KEY (requiredS_id) REFERENCES requiredS (id)
+      FOREIGN KEY (hide) REFERENCES requiredS (hide)
     );
     `);
 
