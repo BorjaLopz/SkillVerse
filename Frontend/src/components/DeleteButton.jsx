a6675ede95569da8888426f2db27da76d2552333
Ana Belén <ana.b.95@hotmail.com>
2023-06-13 19:13:33 +0200

    Componentes, main, pages

---
+++ Frontend/src/components/DeleteButton.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const DeleteButton = () => {
+  return (
+    <div>
+      <button className="delete_button">Delete account</button>
+    </div>
+  );
+};
+
+export default DeleteButton;
--- Frontend/src/pages/Profile.jsx
+++ Frontend/src/pages/Profile.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import Avatar from "../components/Avatar/index";
+import DeleteButton from "../components/DeleteButton";
 
 function Profile() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Profile</h1>
+      <h1 className="text-3xl font-bold underline">User profile</h1>
+      <section className="user_info">
+        <h2>My profile info</h2>
+        <Avatar />
+      </section>
+      <section className="delete_user">
+        <h2>Delete account</h2>
+        <DeleteButton />
+      </section>
     </>
   );
 }
