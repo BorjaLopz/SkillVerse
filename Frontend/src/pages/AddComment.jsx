8781ee85e74df95fb3e05c9bc3d037ad6b57de00
Monica <moirivilla@gmail.com>
2023-06-17 13:54:32 +0200

    Components CommentCard y CommentForm

--- Frontend/src/pages/AddComment.jsx
+++ Frontend/src/pages/AddComment.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function AddComment() {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Añade un servicio</h1>
+      <h1 className="text-3xl font-bold underline">Añade un comentario</h1>
     </>
   );
 }
