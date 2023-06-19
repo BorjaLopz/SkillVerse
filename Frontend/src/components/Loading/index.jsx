897693d6443601b92112386a5d73f329d4f9053c
Ana Belén <ana.b.95@hotmail.com>
2023-06-19 21:07:28 +0200

    loading, alguna cosilla más, no me acuerdo

---
+++ Frontend/src/components/Loading/index.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "style.css";
+
+const Loading = () => {
+  return (
+    <div className="spinner-container">
+      <div className="loading-spinner"></div>
+    </div>
+  );
+};
+
+export default Loading;
+
+// Optional code to simulate delay
+// setTimeout(() => {
+//   setUsers(respose.data);
+//   setIsLoading(false);
+// }, 1000);
