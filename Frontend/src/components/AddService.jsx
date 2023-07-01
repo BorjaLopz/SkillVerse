abcf043c869553f67bbbbde5ac8dfe245116155e
Ana Belén <ana.b.95@hotmail.com>
2023-07-01 13:53:24 +0200

    toast AddService

--- Frontend/src/components/AddService.jsx
+++ Frontend/src/components/AddService.jsx
@@ -122,7 +122,7 @@ const AddService = () => {
           Subir archivo:
         </label>
         <input
-          className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+          className="w-full px-3 py-2 border border-gray-300 rounded-lg"
           aria-describedby="file_help"
           id="file"
           type="file"
