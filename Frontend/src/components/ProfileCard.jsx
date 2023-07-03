b94f7c7280d61c6bedbf5847e9a460937c403987
Ana Belén <ana.b.95@hotmail.com>
2023-07-03 13:50:19 +0200

    Arreglos varios

--- Frontend/src/components/ProfileCard.jsx
+++ Frontend/src/components/ProfileCard.jsx
@@ -2,21 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 import koFiIcon from "/icons/ko-fi-icon.svg";
 
-const ProfileCard = ({ formData}) => {
-  console.log(formData)
+const ProfileCard = ({ formData }) => {
   return (
     <div className="profile-card">
       <h3>Datos Personales</h3>
+      <p>Nickname: {formData.nickname}</p>
       <p>Email: {formData.email}</p>
       <p>Foto de usuario: {formData.userPhoto}</p>
-      <p>Nickname: {formData.nickname}</p>
       <p>Nombre: {formData.name}</p>
       <p>Apellido: {formData.surname}</p>
       <p>Biografía: {formData.biography}</p>
 
-  <a href={formData.ko_fi} target="_blank" rel="noopener noreferrer">
-  <img src={koFiIcon} alt="Ko-fi" style={{ width: "40px", height: "40px" }} />
-</a>
+      <a href={formData.ko_fi} target="_blank" rel="noopener noreferrer">
+        <img
+          src={koFiIcon}
+          alt="Ko-fi"
+          style={{ width: "40px", height: "40px" }}
+        />
+      </a>
 
       <div>
         <Link to={`/user/${formData.nickname}`}>Ver perfil</Link>
@@ -26,5 +29,3 @@ const ProfileCard = ({ formData}) => {
 };
 
 export default ProfileCard;
-
-
