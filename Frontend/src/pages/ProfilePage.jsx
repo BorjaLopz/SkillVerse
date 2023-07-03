2a50c1763241146725ea91101e6aeccba1a3c52e
Ana Belén <ana.b.95@hotmail.com>
2023-07-03 19:32:41 +0200

    Loading, favicon

--- Frontend/src/pages/ProfilePage.jsx
+++ Frontend/src/pages/ProfilePage.jsx
@@ -2,19 +2,14 @@ import React from "react";
 import Profile from "../components/Profile";
 import { useParams } from "react-router";
 
-
 function ProfilePage() {
   const { nickname } = useParams();
 
   return (
     <main>
       <Profile user={nickname} />
-    
     </main>
   );
 }
 
 export default ProfilePage;
-
-
-
