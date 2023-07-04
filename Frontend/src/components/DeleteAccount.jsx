68995ea984a995a98f7795f605287d5892f58106
Ana Belén <ana.b.95@hotmail.com>
2023-07-04 12:18:24 +0200

    Delete comment, service y profile -> No funcionan

--- Frontend/src/components/DeleteAccount.jsx
+++ Frontend/src/components/DeleteAccount.jsx
@@ -1,6 +1,25 @@
-function DeleteAccount() {
+import React from "react";
+import toast from "react-hot-toast";
+import useServer from "../hooks/useServer";
+
+function DeleteAccount({ accountId, onDelete }) {
+  const { delete: deleteAccount } = useServer();
+
+  const handleDelete = async () => {
+    try {
+      await deleteAccount({ url: `/profile/${accountId}` });
+      toast.success("Usuario borrado exitosamente");
+      onDelete();
+    } catch (error) {
+      toast.error("Error al borrar el usuario");
+    }
+  };
+
   return (
-    <button className="publish-comment text-white font-bold py-2 px-4 rounded content-center bg-indigo-500 hover:bg-red-900">
+    <button
+      className="publish-comment text-white font-bold py-2 px-4 rounded content-center bg-indigo-500 hover:bg-red-900"
+      onClick={handleDelete}
+    >
       Borrar cuenta
     </button>
   );
--- Frontend/src/components/DeleteComment.jsx
+++ Frontend/src/components/DeleteComment.jsx
@@ -1,6 +1,25 @@
-function DeleteComment() {
+import React from "react";
+import toast from "react-hot-toast";
+import useServer from "../hooks/useServer";
+
+function DeleteComment({ commentId, onDelete }) {
+  const { delete: deleteComment } = useServer();
+
+  const handleDelete = async () => {
+    try {
+      await deleteComment({ url: `/comments/${commentId}` });
+      toast.success("Comentario borrado exitosamente");
+      onDelete();
+    } catch (error) {
+      toast.error("Error al borrar el comentario");
+    }
+  };
+
   return (
-    <button className="publish-comment text-white font-bold py-2 px-4 rounded content-center bg-indigo-500 hover:bg-red-900">
+    <button
+      className="publish-comment text-white font-bold py-2 px-4 rounded content-center bg-indigo-500 hover:bg-red-900"
+      onClick={handleDelete}
+    >
       Borrar comentario
     </button>
   );
